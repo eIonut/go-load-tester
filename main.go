5ab48b0f38210bebb8eba70a1cfb9ae5b0e0d37c
Gabriel Ionut Enache <enacheionut2399@gmail.com>
2026-08-19 20:19:16 +0300

    CLI Input Version 1

--- main.go
+++ main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"flag"
+	"fmt"
+)
 
-func main (){
-	fmt.Println("Hello")
+type LoadTestArgs struct {
+	requestsNumber *int
+	workersNumber  *int
+	targetURL      *string
+}
+
+type UserInput struct {
+	requestsNumber int
+	workersNumber  int
+	targetURL      string
+}
+
+const DEFAULT_REQUESTS_NUMBER = 100
+const DEFAULT_WORKERS_NUMBER = 10
+
+func main() {
+
+	requestsNumber := flag.Int("requests", DEFAULT_REQUESTS_NUMBER, "Total number of requests to be executed")
+	targetURL := flag.String("url", "", "Target URL to stress test")
+	workersNumber := flag.Int("workers", DEFAULT_WORKERS_NUMBER, "The number of workers to execute jobs")
+
+	flag.Parse()
+
+	userInput, err := readUserInput(LoadTestArgs{requestsNumber: requestsNumber, targetURL: targetURL, workersNumber: workersNumber})
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Println(userInput)
+}
+
+func readUserInput(input LoadTestArgs) (UserInput, error) {
+
+	if *input.workersNumber <= 0 || *input.requestsNumber <= 0 {
+		return UserInput{}, errors.New("You must supply a number of workers and requests > 0")
+	}
+
+	if len(*input.targetURL) == 0 {
+		return UserInput{}, errors.New("No URL supplied")
+	}
+
+	return UserInput{requestsNumber: *input.requestsNumber, targetURL: *input.targetURL, workersNumber: *input.workersNumber}, nil
 }
