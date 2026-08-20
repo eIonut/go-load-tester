32e7462f9f2fe016ca010036a3717a59b68dbdb9
Gabriel Ionut Enache <enacheionut2399@gmail.com>
2026-08-20 15:20:01 +0300

    finish load tester v1

--- main.go
+++ main.go
@@ -21,9 +21,11 @@ type UserInput struct {
 }
 
 type LoadTestStatistics struct {
-	statusCode  int
-	elapsedTime time.Duration
-	err         error
+	statusCode      int
+	elapsedTime     time.Duration
+	successfulCount int
+	failureCount    int
+	err             error
 }
 
 type LoadTestSummary struct {
@@ -31,8 +33,7 @@ type LoadTestSummary struct {
 	successRequests int
 	failedRequests  int
 	averageLatency  time.Duration
-	minLatency      time.Duration
-	maxLatency      time.Duration
+	totalDuration   time.Duration
 }
 
 const defaultRequestsNumber = 100
@@ -51,54 +52,99 @@ func readUserInput(input LoadTestArgs) (UserInput, error) {
 	return UserInput{requestsNumber: *input.requestsNumber, targetURL: *input.targetURL, workersNumber: *input.workersNumber}, nil
 }
 
-func makeApiRequest(url string, ch chan LoadTestStatistics) {
+func makeApiRequest(url string) LoadTestStatistics {
 	start := time.Now()
 
 	resp, err := http.Get(url)
 
 	if err != nil {
-		ch <- LoadTestStatistics{
-			err: err,
+		fmt.Println("Err:", err)
+		return LoadTestStatistics{
+			failureCount:    1,
+			successfulCount: 0,
+			err:             err,
 		}
-		return
 	}
 
 	defer resp.Body.Close()
 
-	elapsedTime := time.Since(start)
+	return LoadTestStatistics{
+		statusCode:      resp.StatusCode,
+		elapsedTime:     time.Since(start),
+		failureCount:    0,
+		successfulCount: 1,
+	}
+}
 
-	ch <- LoadTestStatistics{statusCode: resp.StatusCode, elapsedTime: elapsedTime, err: nil}
+func worker(jobs <-chan struct{}, results chan<- LoadTestStatistics, url string) {
+	for range jobs {
+		result := makeApiRequest(url)
+		results <- LoadTestStatistics{successfulCount: result.successfulCount, failureCount: result.failureCount, err: result.err, statusCode: result.statusCode, elapsedTime: result.elapsedTime}
+	}
+}
+
+func aggregateStatistics(statistics []LoadTestStatistics) LoadTestSummary {
+	summary := LoadTestSummary{totalRequests: len(statistics)}
+
+	var totalLatency time.Duration
+
+	for _, s := range statistics {
+		summary.successRequests += s.successfulCount
+		summary.failedRequests += s.failureCount
+
+		if s.err == nil {
+			totalLatency += s.elapsedTime
+		}
+	}
+
+	summary.totalDuration = totalLatency
+
+	if summary.successRequests > 0 {
+		summary.averageLatency = totalLatency / time.Duration(summary.successRequests)
+	}
+
+	return summary
 }
 
 func main() {
-	statistics := make(chan LoadTestStatistics)
-
 	requestsNumber := flag.Int("requests", defaultRequestsNumber, "Total number of requests to be executed")
 	targetURL := flag.String("url", "", "Target URL to stress test")
 	workersNumber := flag.Int("workers", defaultWorkersNumber, "The number of workers to execute jobs")
-
 	flag.Parse()
 
 	userInput, err := readUserInput(LoadTestArgs{requestsNumber: requestsNumber, targetURL: targetURL, workersNumber: workersNumber})
-
 	if err != nil {
-		fmt.Println("Error:", err)
+		fmt.Println("Error:", err.Error())
 		return
 	}
 
-	results := make([]LoadTestStatistics, 0, userInput.requestsNumber)
-
+	jobs := make(chan struct{}, userInput.requestsNumber)
 	for range userInput.requestsNumber {
-		go makeApiRequest(userInput.targetURL, statistics)
+		jobs <- struct{}{}
+	}
+	close(jobs)
+
+	results := make(chan LoadTestStatistics)
+	workers := min(userInput.workersNumber, userInput.requestsNumber)
+	statistics := make([]LoadTestStatistics, 0, userInput.requestsNumber)
+
+	for range workers {
+		go worker(jobs, results, userInput.targetURL)
 	}
 
-	for range userInput.requestsNumber {
-		results = append(results, <-statistics)
+	for i := 0; i < userInput.requestsNumber; i++ {
+		statistics = append(statistics, <-results)
 	}
 
-	//TODO: add a summary
-	for _, res := range results {
-		fmt.Println(res)
-	}
+	summary := aggregateStatistics(statistics)
+
+	fmt.Println("Load test completed")
+	fmt.Println("")
+	fmt.Println("Total requests:", summary.totalRequests)
+	fmt.Println("Successful:", summary.successRequests)
+	fmt.Println("Failed:", summary.failedRequests)
+	fmt.Println("")
+	fmt.Println("Total duration:", summary.totalDuration)
+	fmt.Println("Average latency:", summary.averageLatency)
 
 }
